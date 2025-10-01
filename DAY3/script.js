4b107119649d0ff9ea17ff11354890a3a3afb7f0
sarrthak008 <sarthaknavale08@gmail.com>
2025-10-01 21:18:00 +0530

    learn about time line

---
+++ DAY3/script.js
@@ -0,0 +1,86 @@
+
+let tl = gsap.timeline();
+
+
+ tl.from("nav",{
+    y:-100,
+    duration:0.5,
+    opacity:0
+ })
+
+ tl.from(".img-container",{
+    y:-30,
+    duration:0.5,
+    opacity:0,
+ })
+
+ tl.from(".heading",{
+    y:-30,
+    duration:0.5,
+    opacity:0
+ })
+
+ tl.from(".paragraph",{
+    y:-30,
+    duration:0.5,
+    opacity:0
+ })
+
+
+ tl.from(".img1",{
+    top:"100%",
+    left:"0%",
+    duration:0.4,
+    opacity:0
+ },"part1")
+
+ tl.from(".img2",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part2")
+
+
+  tl.from(".img3",{
+    top:"100%",
+    right:"0%",
+    duration:0.5,
+    opacity:0
+ },"part2")
+
+  tl.from(".img4",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part2")
+
+  tl.from(".img5",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part1")
+
+  tl.from(".img6",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part1")
+
+  tl.from(".img7",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part2")
+
+  tl.from(".img8",{
+    top:"100%",
+    right:"0%",
+    duration:0.4,
+    opacity:0
+ },"part1")
+
